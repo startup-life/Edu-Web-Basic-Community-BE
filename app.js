999f9377dbf232672bef133c4e1fa304a810e62e
RyeinKim <ryein6896@gmail.com>
2025-05-26 22:21:55 +0900

    미니퀘스트 - Express.js 설치 - 2

---
+++ app.js
@@ -0,0 +1,19 @@
+// express 모듈을 불러옵니다.
+const express = require('express');
+// express 애플리케이션을 생성합니다.
+const app = express();
+// 웹 서버가 사용할 포트 번호를 정의합니다.
+const port = 3000;
+
+/**
+ * 루트 경로('/')에 대한 GET 요청을 처리
+ * 요청이 오면 'Hello World!' 문자열을 응답
+ */
+app.get('/', (request, response) => {
+    // 응답.보내다('Hello World!');
+    return response.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
