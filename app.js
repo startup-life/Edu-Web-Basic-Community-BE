0eeefe3f4afd6769efdad6eefda86e55f1924213
RyeinKim <ryein6896@gmail.com>
2025-05-27 09:52:33 +0900

    미니퀘스트 - 디자인패턴 - 2

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 // express 모듈을 불러옵니다.
 const express = require('express');
-
-// connect-timeout 모듈을 불러옵니다.
-const timeout = require('connect-timeout');
+// express-rate-limit 모듈을 불러옵니다.
+const rateLimit = require('express-rate-limit');
 
 // express 애플리케이션을 생성합니다.
 const app = express();
@@ -10,26 +9,26 @@ const app = express();
 // 웹 서버가 사용할 포트 번호를 정의합니다.
 const port = 3000;
 
-// 요청 타임아웃 설정 (예: 5초)
-app.use(timeout('5s'));
-
-// 라우트 정의
-app.get('/', (request, response) => {
-    // 10초 후 응답을 보냅니다.
-    setTimeout(() => {
-        response.send('Hello World!');
-    }, 10000);
+// 요청 속도 제한 설정
+const limiter = rateLimit({
+    // 1분 동안
+    windowMs: 1 * 60 * 1000,
+    // 최대 5개의 요청을 허용
+    max: 5,
+    // 제한 초과 시 전송할 메시지
+    message: 'too_many_requests',
+    // RateLimit 헤더 정보를 표준으로 사용할지 여부
+    standardHeaders: true,
+    // 레거시 X-RateLimit 헤더를 제거할지 여부
+    legacyHeaders: false,
 });
 
-// 타임아웃 발생 시 처리 핸들러
-app.use((request, response, next) => {
-    if (!request.timedout) next();
-    else
-        response.status(503).send({
-            status: 503,
-            message: 'Request_timeout',
-            data: null,
-        });
+// 모든 요청에 대해 요청 속도 제한 적용
+app.use(limiter);
+
+// 라우트 정의
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
 });
 
 app.listen(port, () => {
