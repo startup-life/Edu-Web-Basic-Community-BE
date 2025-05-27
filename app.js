736c0c9c8f44d17b3a29a54c6016405a8fa82e5d
RyeinKim <ryein6896@gmail.com>
2025-05-27 09:54:03 +0900

    미니퀘스트 - 디자인패턴 - 3

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // express 모듈을 불러옵니다.
 const express = require('express');
-// express-rate-limit 모듈을 불러옵니다.
-const rateLimit = require('express-rate-limit');
+// helmet 모듈을 불러옵니다.
+const helmet = require('helmet');
 
 // express 애플리케이션을 생성합니다.
 const app = express();
@@ -9,26 +9,12 @@ const app = express();
 // 웹 서버가 사용할 포트 번호를 정의합니다.
 const port = 3000;
 
-// 요청 속도 제한 설정
-const limiter = rateLimit({
-    // 1분 동안
-    windowMs: 1 * 60 * 1000,
-    // 최대 5개의 요청을 허용
-    max: 5,
-    // 제한 초과 시 전송할 메시지
-    message: 'too_many_requests',
-    // RateLimit 헤더 정보를 표준으로 사용할지 여부
-    standardHeaders: true,
-    // 레거시 X-RateLimit 헤더를 제거할지 여부
-    legacyHeaders: false,
-});
-
-// 모든 요청에 대해 요청 속도 제한 적용
-app.use(limiter);
+// helmet 미들웨어를 적용합니다.
+app.use(helmet());
 
 // 라우트 정의
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.get('/', (request, response) => {
+    return response.send('Hello, World!');
 });
 
 app.listen(port, () => {
