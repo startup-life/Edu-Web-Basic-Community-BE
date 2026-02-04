fea05e5cc899ff8e35354351f855e5260931c643
RyeinKim <ryein6896@gmail.com>
2026-02-04 09:26:33 +0900

    refactor: Trasaction을 컨트롤할 수 있는 함수 추가

--- databases/index.js
+++ databases/index.js
@@ -21,4 +21,25 @@ const query = async (queryString, params = []) => {
     return rows;
 };
 
-module.exports = { config, pool, query };
+const withTransaction = async work => {
+    const connection = await pool.getConnection();
+    const originalExecute = connection.execute.bind(connection);
+    connection.execute = async (...args) => {
+        if (args[0]) console.log(colors.yellow(args[0]));
+        return originalExecute(...args);
+    };
+    try {
+        await connection.beginTransaction();
+        const result = await work(connection);
+        await connection.commit();
+        return result;
+    } catch (error) {
+        await connection.rollback();
+        throw error;
+    } finally {
+        connection.execute = originalExecute;
+        connection.release();
+    }
+};
+
+module.exports = { config, pool, query, withTransaction };
